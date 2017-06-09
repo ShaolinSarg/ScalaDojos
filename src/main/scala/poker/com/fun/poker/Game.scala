c8f40cd52ea9d07dbb1f571784a3cfb9b49d7cf9
David Sarginson <david.sarginson@digital.hmrc.gov.uk>
2017-06-09 15:56:55 +0100

    week 11 poker dojo

--- src/main/scala/poker/com/fun/poker/Game.scala
+++ src/main/scala/poker/com/fun/poker/Game.scala
@@ -2,18 +2,36 @@ package poker.com.fun.poker
 
 import poker.CardSet
 
+import scala.collection.immutable.Stream.Empty
+
 case class Game(hands: Map[Int, CardSet]) {
   def winner = {
-    val playerOneCards = hands(1)
+    val playerOneCards: CardSet = hands(1)
     val playerTwoCards = hands(2)
+    val playerOnePairs: Set[Set[Card]] = playerOneCards.getPairs
+    val playerTwoPairs: Set[Set[Card]] = playerTwoCards.getPairs
 
-    if (playerOneCards == CardSet.highHand(playerOneCards, playerTwoCards)){
-      Player(1)
-    }else
-      Player(2)
+    (playerOnePairs.isEmpty, playerTwoPairs.isEmpty) match {
+      case (true, true) =>
+        if (playerOneCards == CardSet.highHand(playerOneCards, playerTwoCards)) {
+          Player(1)
+        } else
+          Player(2)
 
+      case (playerOneHasPair, true) => Player(1)
+      case (true, playerTwoHasPair) => Player(2)
+      case (playerOneHasPair, playerTwoHasPair) =>
+
+        if (playerOnePairs.size > playerTwoPairs.size) Player(1)
+        else if (playerOnePairs.size < playerTwoPairs.size) Player(2)
+        else {
+          if (playerOneCards == CardSet.highHand(CardSet(playerOnePairs.head), CardSet(playerTwoPairs.head))) {
+            Player(1)
+          } else
+            Player(2)
+        }
+    }
   }
-
 }
 
 object Game {
--- src/test/scala/poker/GameSpec.scala
+++ src/test/scala/poker/GameSpec.scala
@@ -22,7 +22,7 @@ class GameSpec extends WordSpec with Matchers{
       val playerCards: Game = Game.start(2)
       playerCards.hands(1) should not be playerCards.hands(2)
     }
-    "tell us which player has won" in {
+    "tell us which player has won when the best hand is a high card" in {
       val player1Cards = CardSet(Set(Card(Ace, Hearts), Card(Two, Clubs)))
       val player2Cards = CardSet(Set(Card(King, Hearts), Card(Three, Clubs)))
       val game = Game(Map(1 -> player1Cards, 2 -> player2Cards))
@@ -32,6 +32,40 @@ class GameSpec extends WordSpec with Matchers{
       val game2 = Game(Map(2 -> player1Cards, 1 -> player2Cards))
 
       game2.winner shouldBe Player(2)
+    }
+
+    "tell us which player has won when the best hand is a pair against a high card" in {
+      val player1Cards = CardSet(Set(Card(Two, Hearts), Card(Two, Clubs)))
+      val player2Cards = CardSet(Set(Card(Ace, Hearts), Card(Three, Clubs)))
+      val game = Game(Map(1 -> player1Cards, 2 -> player2Cards))
+
+      game.winner shouldBe Player(1)
+
+      val game2 = Game(Map(2 -> player1Cards, 1 -> player2Cards))
+
+      game2.winner shouldBe Player(2)
+
+    }
+
+    "tell us which player has won when the best hand is a pair against a pair" in {
+      val player1Cards = CardSet(Set(Card(Two, Hearts), Card(Two, Clubs)))
+      val player2Cards = CardSet(Set(Card(Three, Hearts), Card(Three, Clubs)))
+      val game = Game(Map(2 -> player1Cards, 1 -> player2Cards))
+
+      game.winner shouldBe Player(1)
+
+      val game2 = Game(Map(1 -> player1Cards, 2 -> player2Cards))
+
+      game2.winner shouldBe Player(2)
+    }
+
+    "tell us which player has won when the best hand is a pair against a two pair" in {
+      val player1Cards = CardSet(Set(Card(Two, Hearts), Card(Two, Clubs), Card(Three, Hearts), Card(Three, Clubs)))
+      val player2Cards = CardSet(Set(Card(Four, Hearts), Card(Four, Clubs)))
+
+      val game = Game(Map(1 -> player1Cards, 2 -> player2Cards))
+
+      game.winner shouldBe Player(1)
     }
 
     "tell us what hand the winner had" in {
